4c70e9aa11ea6fbde4c01d45d8826615afee8e99
Lars Cormann <lars.cormann@obi.de>
2021-04-14 17:02:59 +0200

    Initial commit

---
+++ consumers/faust_stream.py
@@ -0,0 +1,59 @@
+"""Defines trends calculations for stations"""
+import logging
+
+import faust
+
+logger = logging.getLogger(__name__)
+
+
+# Faust will ingest records from Kafka in this format
+class Station(faust.Record):
+    stop_id: int
+    direction_id: str
+    stop_name: str
+    station_name: str
+    station_descriptive_name: str
+    station_id: int
+    order: int
+    red: bool
+    blue: bool
+    green: bool
+
+
+# Faust will produce records to Kafka in this format
+class TransformedStation(faust.Record):
+    station_id: int
+    station_name: str
+    order: int
+    line: str
+
+
+app = faust.App("stations-stream", broker="kafka://localhost:9092", store="memory://")
+topic = app.topic("org.chicago.cta.stations", value_type=Station)
+out_topic = app.topic("org.chicago.cta.stations.table.v1", partitions=1)
+table = app.Table(
+    "transformed_stations",
+    default=TransformedStation,
+    partitions=1,
+    changelog_topic=out_topic,
+)
+
+
+@app.agent(topic)
+async def transformer(stream):
+    async for value in stream:
+        logger.info(f'RECEIVED -- {value!r}')
+        if value.red:
+            line = "red"
+        elif value.blue:
+            line = "blue"
+        elif value.green:
+            line = "green"
+        else:
+            line = None
+        transformed_station = TransformedStation(station_id=value.station_id, station_name=value.station_name, order=value.order, line=line)
+        table[value.station_id] = transformed_station
+
+
+if __name__ == "__main__":
+    app.main()
